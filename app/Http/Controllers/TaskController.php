9f3e6ce84418ee58477dde2f66e132cac493011d
t-kubrak <chikashikaari@gmail.com>
2020-08-21 10:58:00 +0300

    log task operations

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,10 +7,13 @@ namespace App\Http\Controllers;
 use App\Board;
 use App\Jobs\ProcessImage;
 use App\Label;
+use App\Log;
 use App\Task;
 use App\TaskLabel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
@@ -44,6 +47,8 @@ class TaskController extends \Illuminate\Routing\Controller
         $task->board_id = $request->board_id;
         $task->save();
 
+        $this->log('create-task', $task);
+
         return response()->json($task);
     }
 
@@ -56,6 +61,8 @@ class TaskController extends \Illuminate\Routing\Controller
         $taskLabel->task_id = $taskId;
         $taskLabel->label_id = $labelId;
         $taskLabel->save();
+
+        $this->log('attach-label', $taskLabel);
 
         return response()->json($taskLabel);
     }
@@ -76,5 +83,17 @@ class TaskController extends \Illuminate\Routing\Controller
         ProcessImage::dispatch($imageTempFilePath, $taskId);
 
         return response()->json();
+    }
+
+    private function log(string $operation, Model $entity): void
+    {
+        $log = new Log();
+
+        $log->operation = $operation;
+        $log->user_id = Auth::id();
+        $log->entity = get_class($entity);
+        $log->object = $entity->toArray();
+
+        $log->save();
     }
 }
